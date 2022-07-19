ccf9d954f1138ca5c6acf5dd00d45fc7ae958d74
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-19 01:06:14 -0700

    CH5-E arrays, alst question

--- ruby_prep/ch5-arrays/e_arrays.rb
+++ ruby_prep/ch5-arrays/e_arrays.rb
@@ -59,3 +59,17 @@ cats = ["Burbus","Clarice","Motita","Nieve","Vainilla"]
 cats.each_with_index do |cat, idx|
   puts "#{cat} is at index #{idx}"
 end
+
+# E8 Write a program that iterates over an array and builds a new array that is the result of
+# incrementing each value in the original array by a value of 2. You should have two arrays at 
+# the end of this program, The original array and the new array you've created. Print both arrays 
+# to the screen using the p method instead of puts.
+
+arre = [2,4,6,8]
+
+new_arre = arre.map do |item|
+              item * 2
+           end
+
+p arre
+p new_arre
