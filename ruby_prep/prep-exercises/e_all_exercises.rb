714c88d1e6630d085a540671d0e443f88023fcd5
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-19 02:48:01 -0700

    E all exercises in the prep book

--- ruby_prep/prep-exercises/e_all_exercises.rb
+++ ruby_prep/prep-exercises/e_all_exercises.rb
@@ -56,9 +56,82 @@ hashy2 = {:name => "rosa"}
 
 # E9 Suppose you have a hash h = {a:1, b:2, c:3, d:4}
 # a) Get the value of key `:b`.
+h = {a:1, b:2, c:3, d:4}
+p h[:b]
 
 # b) Add to this hash the key:value pair `{e:5}`
-
+h = {a:1, b:2, c:3, d:4}
+h[:e] = 5
+p h 
 # c) Remove all key:value pairs whose value is less than 3.5
-
+h = {a:1, b:2, c:3, d:4}
+h.delete_if { |k, v| v < 3.5 }
+p h
 # E10 Can hash values be arrays? Can you have an array of hashes? (give examples)
+# Yes, hash values can be arrays. 
+# You can have an array of hahes. 
+ex1 = {name: ["rosa","isela"]}
+p ex1
+
+ex2 = [{name: "rosa", surname:"soria"}]
+p ex2
+
+# E11 Given the following data structures, write a program that copies the information
+# from the array into the empty hash that applies to the correct person.
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+# Expected output:
+#  {
+#    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
+
+p contacts
+
+# E12 Using the hash you created from the previous exercise, demonstrate how you would
+# access Joe's email and Sally's phone number.
+
+p contacts["Sally Johnson"][:phone]
+p contacts["Joe Smith"][:email]
+
+# E13 use delete_if? and start_with? to delete all the stirngs that start with s
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if {|item| item.start_with?("s")}
+p arr
+
+
+arr.delete_if {|item| item.start_with?("s","w")}
+
+p arr
+
+# E14 given the following 
+a = ['white snow', 'winter wonderland', 'melting ice','slippery sidewalk', 'salted roads', 'white trees']
+
+new_arre = a.map do |term|
+            term.split(" ")
+          end
+p new_arre.flatten
+
+
+# E15 what will the following output:
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+# this outputs true beacause hashes have no order
