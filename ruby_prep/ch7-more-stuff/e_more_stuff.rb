6fe0b54a61644d7ee305f14b0e31818781e89578
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-19 02:09:00 -0700

    CH7-E extra stuff

--- ruby_prep/ch7-more-stuff/e_more_stuff.rb
+++ ruby_prep/ch7-more-stuff/e_more_stuff.rb
@@ -12,3 +12,36 @@ end
 arre.each do |item|
   checker(item)
 end
+
+# E2 What will the following print and what will it return?
+def execute(&block)
+  block
+end
+
+execute { puts "Hello from inside the execute method!" }
+
+# nothing gets printed because block never gets called with `call` method
+# and the method returns a proc
+
+# E3 What is exception handling and what prblem does it solve?
+# Exception handling allows for a program to contiue to execute without stopping 
+# when it encounters an error. It is useful because it alos logs error
+
+# E4 Modify the code in exercise 2 to make the block execute properly
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
+
+# E5 Why does the following code give the following error:
+def execute(block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
+
+block.rb1:in `execute': wrong number of arguments (0 for 1) (ArgumentError)
+from test.rb:5:in `<main>'
+
+# the way we are passing the block is the issue, it should use an ampersand &block
