26527b4d79275117006c46656ca9e585d736359b
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-26 14:29:13 -0700

    E- refactoring calc to use prompt method

--- rb101/calculator/calculator.rb
+++ rb101/calculator/calculator.rb
@@ -1,21 +1,27 @@
-Kernel.puts('Welcome to the calculator!')
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
 
-Kernel.puts("What's the first number?")
+prompt('Welcome to the calculator!'
+)
+prompt("What's the first number?")
 number1 = Kernel.gets().chomp()
 
-Kernel.puts("What's the second number?")
+prompt("What's the second number?")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) mnultiply 4) divide")
+prompt("What operation would you like to perform? 1) add 2) subtract 3) mnultiply 4) divide")
 
 operator = Kernel.gets().chomp()
 
 if operator == '1'
   result = number1.to_i() + number2.to_i()
 elsif operator == '2'
-  result = number.to_i() - number2.to_i()
+  result = number1.to_i() - number2.to_i()
 elsif operator == '3'
-  result = number.to_i() * number2.to_i()
+  result = number1.to_i() * number2.to_i()
 else 
-  result = number.to_f() / number2.to_f()
+  result = number1.to_f() / number2.to_f()
 end
+
+prompt("The result is #{result}")
