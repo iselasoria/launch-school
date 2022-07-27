eddfa3b7f919763e53f40b32a74ba9d514386954
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-27 04:25:51 -0700

    E- refactoring calc validate input

--- rb101/calculator/calculator.rb
+++ rb101/calculator/calculator.rb
@@ -2,27 +2,40 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def valid_number?(num)
+  num.to_i() != 0
+end
+
 prompt('Welcome to the calculator!'
 )
-prompt("What's the first number?")
-number1 = Kernel.gets().chomp()
 
-prompt("What's the second number?")
-number2 = Kernel.gets().chomp()
+number1 = '' 
+loop do
+  prompt("What's the first number?")
+  number1 = Kernel.gets().chomp()
 
+  if valid_number?(number1)
+    break
+  else
+    prompt("Hmm..that doesn't look like a valid number")
+  end
+end
+
+number2 = ''
+loop do
+  prompt("What's the second number?")
+  number2 = Kernel.gets().chomp()
+
+  if valid_number?(number2)
+    break
+  else
+    prompt("Hmm that doesn't look like a valid number")
+  end
+
+end
 prompt("What operation would you like to perform? 1) add 2) subtract 3) mnultiply 4) divide")
 
 operator = Kernel.gets().chomp()
-
-# if operator == '1'
-#   result = number1.to_i() + number2.to_i()
-# elsif operator == '2'
-#   result = number1.to_i() - number2.to_i()
-# elsif operator == '3'
-#   result = number1.to_i() * number2.to_i()
-# else 
-#   result = number1.to_f() / number2.to_f()
-# end
 
 
 result = case operator
