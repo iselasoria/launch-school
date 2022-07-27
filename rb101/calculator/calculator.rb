93433dd9ffb03b7bca93c8f58ce096495174928e
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-27 05:05:45 -0700

    E- refactored calc after rubocop

--- rb101/calculator/calculator.rb
+++ rb101/calculator/calculator.rb
@@ -21,12 +21,12 @@ end
 
 prompt('Welcome to the calculator, enter your name!')
 
-name = ""
+name = ''
 loop do
   name = Kernel.gets().chomp()
 
   if name.empty?()
-    prompt("Make sure you use a valid name")
+    prompt('Make sure you use a valid name')
   else
     break
   end
@@ -35,7 +35,7 @@ end
 prompt("Hi #{name}")
 
 loop do # main loop
-  number1 = '' 
+  number1 = ''
   loop do
     prompt("What's the first number?")
     number1 = Kernel.gets().chomp()
@@ -57,50 +57,47 @@ loop do # main loop
     else
       prompt("Hmm that doesn't look like a valid number")
     end
-
   end
-  prompt("What operation would you like to perform? 1) add 2) subtract 3) mnultiply 4) divide")
 
-  operator_prompt = <<-MSG 
-    What operation would you like to perform? 
-    1) add 
-    2) subtract 
-    3) multiply 
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
     4) divide
   MSG
 
   prompt(operator_prompt)
 
   operator = ''
-  loop do 
+  loop do
     operator = Kernel.gets().chomp()
 
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3 or 4")
+      prompt('Must choose 1, 2, 3 or 4')
     end
   end
 
   prompt("#{operation_to_message(operator)} the two numbers")
 
   result = case operator
-            when '1' 
-              number1.to_i() + number2.to_i()
-            when '2'
-              number1.to_i() - number2.to_i()
-            when '3'
-              number1.to_i() * number2.to_i()
-            when '4'
-              number1.to_f() / number2.to_f()
-  end
+           when '1' 
+            number1.to_i() + number2.to_i()
+           when '2'
+            number1.to_i() - number2.to_i()
+           when '3'
+            number1.to_i() * number2.to_i()
+           when '4'
+            number1.to_f() / number2.to_f()
+           end
 
 
   prompt("The result is #{result}")
-
   prompt("Do you want to perform another calculation? 'Y' to calculate again.")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 end
 
-prompt("Thank you for using the calculator")
+prompt('Thank you for using the calculator')
