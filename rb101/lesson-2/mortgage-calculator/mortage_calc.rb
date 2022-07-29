48752bf8612c238f1cf4c3e80e3eb3f97118870d
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-29 01:15:39 -0700

    P- finishing first iteration mortgage calc

--- rb101/lesson-2/mortgage-calculator/mortage_calc.rb
+++ rb101/lesson-2/mortgage-calculator/mortage_calc.rb
@@ -4,9 +4,10 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-def valid_amount?(amt)
+def valid_number?(amt)
   amt > 0
 end
+
 
 # TODO this is the calculation in RUby
 # m = p * (j / (1 - (1 + j)**(-n)))
@@ -17,19 +18,34 @@ end
 # n = load duration in months
 
 # program flow
-prompt("Welcome to the calculator. How much money do you need to borrow?")
-principal = gets.chomp.to_i
-p valid_amount?(principal)
-p principal
+principal = 0
+
+loop do
+  prompt("Welcome to the calculator. How much money do you need to borrow?")
+  principal = gets.chomp.to_i
+
+  if valid_number?(principal)
+    break
+  else
+    prompt("Hmm that doesn't look like a valid amount, be sure to enter a positive number")
+  end
+end
 
 prompt("What is the interest rate?")
 interest_rate = gets.chomp.to_f
 p interest_rate
 
-prompt("What is the duration of this loan?")
-duration_months = gets.chomp.to_i * 12
-p duration_months
 
+duration_months = 0
+loop do
+  prompt("What is the duration of this loan?")
+  duration_months = gets.chomp.to_i * 12
+  if valid_number?(duration_months)
+    break
+  else
+    prompt("Please enter the years for the duration of you loan:")
+  end
+end
 annual_interest_rate = interest_rate.to_f() / 100
 monthly_interest_rate = annual_interest_rate / 12
 
