a960a602d30eae929aa7de6742d0e2f64074fd55
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-30 01:18:27 -0700

    E- easy1

--- rb101/lesson-3/easy_1.rb
+++ rb101/lesson-3/easy_1.rb
@@ -28,3 +28,28 @@ put ? after something --> this denotes methods that return a boolean; used in lo
 put !! before something, like !!user_name --> this turns any object into its boolean equivalent 
 
 =end
+
+# E3 Replace the word "important" with "urgent" in this string:
+advice = "Few things in life are as important as house training your pet dinosaur."
+advice.gsub!("important","urgent")
+puts advice
+
+# E4 The ruby array class has several kethids for removing items from the array.
+# Two of them have very similar names. How do these differ, what does each do?
+
+numbers = [1,2,3,4,5]
+numbers.delete_at(1)
+numbers.delete(1)
+
+=begin
+SOLUTION
+The method `delete_at` will delete whatever element is at the given array index, in this case index 1.
+So the first method is a destructive method that will remove the umber 2 from the array and it will return
+the number it removed. 
+The second method also destructive will remove the number that it has as an argument, so the number 1. It returns the number it removed.
+The main difference is `delete_at` searches the array for the element at that position, while `delete` searches the array for that value.
+=end
+
+# E5 Programatically determine if 42 lies between 10 and 100.
+(10..100).cover?(42)
+
