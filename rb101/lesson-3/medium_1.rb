b20c65a6fbeffa7073998f4ede8e10559f2d868f
Rosa <rosaiselasoriamonzon@gmail.com>
2022-07-30 02:02:27 -0700

    E- medium 1

---
+++ rb101/lesson-3/medium_1.rb
@@ -0,0 +1,6 @@
+# E1 replicate this, but ten times using a one-liner:
+# The Flintstones Rock!
+#  The Flintstones Rock!
+#   The Flintstones Rock!
+
+10.times {|item| puts (' '* item) + 'The Flintstones Rock!'}
