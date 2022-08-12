cdba22c6d63503531b88f38df3f3e8e60e129650
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-11 22:27:40 -0700

    E- lesson 3 medium 2

--- rb101/lesson-3/medium_2.rb
+++ rb101/lesson-3/medium_2.rb
@@ -11,4 +11,17 @@ puts c.object_id
 # SOLUTION c is pointing to the same object local variable a is 
 # pointing to. local variable b was initialized as a totally different string
 
-# E2
+# E2 what is the outcome of each call to `object_id`?
+
+a = 42
+b = 42
+c = a
+
+puts a.object_id # 123
+puts b.object_id # 123
+puts c.object_id # 123
+
+# SOLUTION
+# in this case, because integers are immutable, all of them reference the same object-- integer 42
+# there is only one integer 42, and that is why they all point to it, even though `b` was not eplicityl
+# initalized to point to it
