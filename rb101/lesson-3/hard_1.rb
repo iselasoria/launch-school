6995ff47dc7d03a09beb86d44138d1f75f3603da
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-12 21:13:26 -0700

    E- lesson 3 hard

--- rb101/lesson-3/hard_1.rb
+++ rb101/lesson-3/hard_1.rb
@@ -10,7 +10,7 @@ greeting
 # SOLUTION 
 # when a local variable is intialized inside an `if` statement, the variable
 # gets initialized to nil even if the block doesnt get executed
-# TODO come back to this, is an if statement a block or not? Why does the variable get initialized to nil???
+# TODO solved--> Pete confirmed if does _not_ create a block.
 
 # E2 what is the result of the last line of code?
 greetings = { a: 'hi' }
@@ -27,9 +27,9 @@ puts greetings # {a: 'hi there'}
 # E3 What is outputted by each of the three code snippets below?
 # SNIPPET A
 def mess_with_vars(one, two, three)
-  one = two # "two"
-  two = three # "three"
-  three = one # one
+  one = two # "one"
+  two = three # "two"
+  three = one # "three"
 end
 
 one = "one"
@@ -43,6 +43,9 @@ puts "two is: #{two}"
 puts "three is: #{three}"
 
 # SOLUTION
+# "one"
+# "two"
+# "three"
 
 # SNIPPET B
 def mess_with_vars(one, two, three)
@@ -62,6 +65,9 @@ puts "two is: #{two}"
 puts "three is: #{three}"
 
 # SOLUTION
+# "one"
+# "two"
+# "three"
 
 # SNIPPET C
 def mess_with_vars(one, two, three)
@@ -81,3 +87,33 @@ puts "two is: #{two}"
 puts "three is: #{three}"
 
 # SOLUTION
+# "two"
+# "three"
+# "one"
+
+# E4 Fix this code so that it identifies valid IP addresses like 10.4.5.11
+# items like these should be invalid: "4.5.5" or "1.2.3.4.5"
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    break unless is_an_ip_number?(word)
+  end
+  return true
+end
+
+# SOLUTION
+# check that the length is 4 items. Breaking out of the loop was just falling
+# through to the return true outside of the loop, so this was a bug too so we
+# can fix by using return false insteaf of break.
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+
+  true
+end
