d65667768107f11d91b2ec04556d34dcd2620d4f
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-18 17:32:55 -0700

    E- small problems

---
+++ exercises/rb101-small-problems/easy-3/odd_lists.rb
@@ -0,0 +1,21 @@
+# write a method that returns an Array that contains every other element of the array that 
+# gets passed in as the argument. The values in the returned list should be those values that are
+# in the 1st, 3rd, 5th and so on elements of the argument array.
+
+def oddities(arre)
+  new_arr = []
+  arre.each_with_index do |item, idx|
+    if idx.even? || idx == 0
+      new_arr << item
+    end
+  end
+  new_arr
+end
+
+p oddities([2, 3, 4, 5, 6])
+
+p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+p oddities(['abc', 'def']) == ['abc']
+p oddities([123]) == [123]
+p oddities([]) == []
