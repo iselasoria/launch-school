2e8c251cc4b2297fe3d80226210cdbe1829fad72
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-20 17:22:31 -0700

    N- collections basics

--- rb101/lesson-4/collection_basics.rb
+++ rb101/lesson-4/collection_basics.rb
@@ -57,3 +57,14 @@ str[15] = 'C'
 str[21] = 'I'
 str[24] = 'B'
 str # => "Joe's Favorite Color Is Blue"
+
+# E8 Given this code, modify each element to increse by 1
+arr = [1, 2, 3, 4, 5]
+arr[0] += 1 # => 2
+arr         # => [2, 2, 3, 4, 5]
+
+arr[1] += 1
+arr[2] += 1
+arr[3] += 1
+arr[4] += 1
+arr # => [2, 3, 4, 5, 6]
