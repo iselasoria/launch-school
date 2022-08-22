fb246e19172b732049e0a77fd5de02d18f5fe775
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-22 15:44:37 -0700

    E- loops while true

---
+++ rb101/lesson-4/loops-1/say_hello.rb
@@ -0,0 +1,20 @@
+# Modify so "Hello!" gets printed 5 times.
+
+say_hello = true
+
+while say_hello
+  puts 'Hello!'
+  say_hello = false
+end
+
+# SOLUTION
+say_hello = true
+iterations = 5
+
+while say_hello
+  puts 'Hello!'
+  iterations += 1
+  if iterations == 5
+    say_hello = false
+  end
+end
