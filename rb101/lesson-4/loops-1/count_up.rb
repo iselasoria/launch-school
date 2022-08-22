088afc6e68f02a6a31ad3c47e975f41283359be2
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-22 15:54:16 -0700

    E- counting up loops

---
+++ rb101/lesson-4/loops-1/count_up.rb
@@ -0,0 +1,17 @@
+# Thus code outputs a countdown from 10 to 1. Modify so that it counts from 1 to 10.
+
+count = 10
+
+until count == 0
+  puts count
+  count -= 1
+end
+
+# SOLUTION
+count = 1
+
+until count == 11
+  puts count
+  count += 1
+end
+
