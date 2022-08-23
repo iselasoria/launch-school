0f3bff037d9a71aa5fd42fa726cf1de6731785aa
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-22 23:01:41 -0700

    E- loops-2 greeting

---
+++ rb101/lesson-4/loops-2/greeting.rb
@@ -0,0 +1,12 @@
+# given the code below, use a `while` loop to pring `Hello!` twice.
+
+def greeting
+  puts 'Hello!'
+end
+
+number_of_greetings = 2
+
+while number_of_greetings > 0
+  greeting
+  number_of_greetings -= 1
+end
