cd4c9dc8983eec216c501f00160305506036caff
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-22 22:11:32 -0700

    E- loops2

---
+++ rb101/lesson-4/loops-2/even_odd.rb
@@ -0,0 +1,10 @@
+# write a loop that prints numbers 1-5 and whether the number is even or odd.
+# Use the code below to get started
+
+count = 1
+
+loop do
+  puts count
+  count += 1
+  break if count > 5
+end
