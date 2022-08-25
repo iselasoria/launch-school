2b7867c7be82cf630786dca7aa24e2b70f1fde9d
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-25 03:04:41 -0700

    E- daily double string manipulation

---
+++ exercises/rb101-small-problems/easy-5/daily_double.rb
@@ -0,0 +1,27 @@
+# write a method that takes a string argument and returns a new string
+# that contains the value of the original string with all consecutive duplicate chars
+# collapsed into a single char. Don't use String#squeeze
+
+
+
+def crunch(str)
+  idx = 0
+  crunch_text = ''
+  while idx <= str.length - 1
+    crunch_text << str[idx] unless str[idx] == str[idx + 1]
+    idx += 1
+  end
+  crunch_text
+end
+
+p crunch('ddaaiillyy ddoouubbllee')
+
+
+# test cases
+# crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+# crunch('4444abcabccba') == '4abcabcba'
+# crunch('ggggggggggggggg') == 'g'
+# crunch('a') == 'a'
+# crunch('') == ''
+
+# todo come back to this one
