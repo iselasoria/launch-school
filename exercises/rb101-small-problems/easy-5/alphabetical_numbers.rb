da5459d652c3077b5ebe91fed208212c2d83b69b
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-25 02:49:34 -0700

    E- alphabetical numbers adding comment

--- exercises/rb101-small-problems/easy-5/alphabetical_numbers.rb
+++ exercises/rb101-small-problems/easy-5/alphabetical_numbers.rb
@@ -18,3 +18,5 @@ p alphabetic_number_sort((0..19).to_a) == [
   8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
   6, 16, 10, 13, 3, 12, 2, 0
 ]
+
+# ? This is not entirely clear, come back to this question.
