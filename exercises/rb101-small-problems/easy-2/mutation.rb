e17b4f242b2d3a211794fe93b54c1b47a1dfcb13
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-27 03:35:19 -0700

    E- small problems easy-2 mutation

--- exercises/rb101-small-problems/easy-2/mutation.rb
+++ exercises/rb101-small-problems/easy-2/mutation.rb
@@ -5,7 +5,7 @@ array2 = []
 
 
 array1.each { |value| array2 << value }
-p array2
+# p array2
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
 puts array2
 
@@ -21,6 +21,6 @@ puts array2
 # Groucho
 # Zeppo
 
-# This is becaise in the first call to each, we use the destructive methoid `<<` to phush each element in array1 into
+# This is because in the first call to each, we use the destructive methoid `<<` to phush each element in array1 into
 # aray2, thus we end up with a copy of array1 but not the same object. Then in the second call to each, we update
 # the array1 in place, therefore now we have that the object referenced by array2 has been changed.
