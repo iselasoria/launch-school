61f77e8f61a8c6e6f493ce7275ffb34574b9ba24
Rosa <rosaiselasoriamonzon@gmail.com>
2022-08-30 18:40:50 -0700

    N- final solution to pedac process example

--- ls-md/pedac-guide/solution.rb
+++ ls-md/pedac-guide/solution.rb
@@ -1,22 +1,46 @@
-# def sum_even_number_row(row_number)
+def sum_even_number_row(row_number)
+  rows = []
+  start_integer = 2
 
-# end
+  (1..row_number).each do |current_row_number|
+    rows << create_row(start_integer, current_row_number)
+    start_integer = rows.last.last + 2
+  end
+
+  # rows.last.reduce { |final_row_sum, num| final_row_sum + num } # this would work too!
+  rows.last.sum
+  
+end
 
 def create_row(start_integer, row_length)
   row = []
+  current_integer = start_integer
   # steps 2-4
+  loop do 
+    row << current_integer
+    current_integer +=2 
+    break if row.length == row_length
+  end
   row
 end
 
-# test cases
-# p sum_even_number_row(1) == 2 # true
-# p sum_even_number_row(2) == 10
-# p sum_even_number_row(4) == 68
+# calculating the start_integer:
+# Rule: first integer of row == last integer of the preceding row + 2
+# Algorithm:
+#   -Get the last row of the rows array
+#   -Get last integer of that row
+#   -Add add 2
 
-p create_row(2, 1)
-p create_row(2, 1) == [2] # true
-p create_row(4, 2) == [4,6]
-p create_row(8, 3) == [8, 10, 12]
+
+# test cases
+p sum_even_number_row(1) == 2 # true
+p sum_even_number_row(2) == 10
+p sum_even_number_row(4) == 68
+
+# p create_row(2, 1)
+# p create_row(2, 1) == [2] # true
+# p create_row(4, 2) == [4,6]
+# p create_row(8, 3) == [8, 10, 12]
 
 # # algo
 # 1. Create an empty 'row' array to contains the integers
@@ -29,3 +53,8 @@ p create_row(8, 3) == [8, 10, 12]
 #   - Add the start integer to the row
 #   - Increment the start integer by 2
 #   - Break out of the loop if length of row == row length
+
+## Final Thoughts
+# Don't approach PEDAC as a linear process, be willing to go back and forth
+# Switch from implementation to back to problem solving mode when necessary
+# Don't try to problem-solve at the code-level
