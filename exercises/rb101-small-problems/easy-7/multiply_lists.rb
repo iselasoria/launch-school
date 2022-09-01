a77c92ef861678b4b7a7eabfe9d3eefd12819d34
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-01 13:24:27 -0700

    E- small problems easy-7 multiplying arrays

---
+++ exercises/rb101-small-problems/easy-7/multiply_lists.rb
@@ -0,0 +1,24 @@
+=begin
+-----------------------INSTRUCTIONS--------------------------------------
+Write a method that takes two array arguments in which each array contains
+a list of numbers, andreturns a new array that contains the product each 
+pair of numbers from the arguments that have the same index.
+You may assume that the arguments contain the same number of elements.
+--------------------------PROBLEM----------------------------------------
+Questions:
+Input: two arrays of numbers
+Output: a new array where each element is the product of the two elements 
+in each of the input arrays that were at the same index
+---------------------------RULES-----------------------------------------
+Explicit: 
+  - the arrays contain the same number of elements
+  - the result is a new array
+Implicit: 
+--------------------------EXAMPLES---------------------------------------
+=end
+# iterate through the first array
+#----------------------------ALGO----------------------------------------
+
+# TODO  
+# test cases
+multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
