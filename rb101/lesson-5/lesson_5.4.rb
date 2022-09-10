f4602caf9e8961411dec913af30f501bc1585883
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-09 18:39:43 -0700

    E- side effects, outputs and returns

--- rb101/lesson-5/lesson_5.4.rb
+++ rb101/lesson-5/lesson_5.4.rb
@@ -11,6 +11,29 @@ end
 |     1     | method call `map`    |Outer Array                           |      None                               |New array`[1,3]`                           | No                                    |
 |    1-4    | block execution      |Each sub-array                        |      None                               |1 & 3, integer at index 0 at each sub-array| Used by map to populate new arr       |
 |     2     | method call `first`  |Each sub-array                        |      None                               |Element at index 0 of each sub-array       | Yes, used by puts method              |
-|     2     | method call `puts`   |Element at index 0 of each subarray   |outputs string representation of integers|        `nil`                              | No                                    |                                       |
+|     2     | method call `puts`   |Element at index 0 of each subarray   |outputs string representation of integers|        `nil`                              | No                                    |
 |     3     | method call `first`  |Each sub-array                        |      None                               |  The value at index 0 of each sub-array   | Used to deterine return value of block|
+=end
+
+# E2 Take apart the following block, noting the side effects, outputs and return values at each step.
+
+my_arr = [[18, 7], [3, 12]].each do |arr|
+  arr.each do |num|
+    if num > 5
+      puts num
+    end
+  end
+end
+
+
+=begin
+| **Line**  | **Action**                                       |**Object**                            |**Side Effect**                          |**Return Value**                           |**Is Return Value used?**                                     |
+| :-------: |:------------------------------------------------:|--------------------------------------|-----------------------------------------|-------------------------------------------|--------------------------------------------------------------|
+|     1     | initialization and assignment of variable my_arr | [[18,7],[3,12]]                      |      None                               |[[18,7],[3,12]]                            | No                                                           |
+|     1     | method call `each`                               | [[18,7],[3,12]]                      |      None                               |The calling object [[18,7],[3,12]]         | Yes, used in variable assignemtn to my_arr                   |
+|    1-7    | outter block execution                           |Each sub-array                        |      None                               |its calling object, each sub-array         | No                                                           |
+|    2-6    | inner block execution                            |Elements inside the sub-array         |      None                               |nil                                        | No                                                           |
+|     3     | comparison  `(>) `                               |Elemet in the sub-array at iteration  |      None                               |boolean                                    | Yes, used by `if`                                            |
+|    3-5    | if statement                                     |The return value of the comparison (<)|      None                               |nil                                        | Yes, used to determine value of inner block                  |
+|     4     | method call `puts`                               |Element of the subarray at iteration  |Outputs string representation of integer |nil                                        | Yes, used to determine return value of conditional statement |
 =end
