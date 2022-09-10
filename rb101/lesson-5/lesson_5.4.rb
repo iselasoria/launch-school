1c46bc07fafd75760c06e281a6ad89534477a024
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-09 18:08:29 -0700

    E- taking apart the block- side effects, outputs, returns

--- rb101/lesson-5/lesson_5.4.rb
+++ rb101/lesson-5/lesson_5.4.rb
@@ -9,7 +9,8 @@ end
 | **Line**  | **Action**           |**Object**                            |**Side Effect**                          |**Return Value**                           |**Is Return Value used?**              |
 | :-------: |:--------------------:|--------------------------------------|-----------------------------------------|-------------------------------------------|---------------------------------------|
 |     1     | method call `map`    |Outer Array                           |      None                               |New array`[1,3]`                           | No                                    |
-|    1-4    | block execution      |Each sub-array                        |      None                               |1 & 3 in each iteration                    |Used by map to populate new arr        |
-|     2     |method call `puts`    |Each sub-array                        |outputs string representation of integers|   `nil`                                   |  No                                   |
-|     3     |  method call `first` |Element at index 0 of each subarray   |      None                               |  The value at indez 0 of each sub-array   | Used to deterine return value of block|
+|    1-4    | block execution      |Each sub-array                        |      None                               |1 & 3, integer at index 0 at each sub-array| Used by map to populate new arr       |
+|     2     | method call `first`  |Each sub-array                        |      None                               |Element at index 0 of each sub-array       | Yes, used by puts method              |
+|     2     | method call `puts`   |Element at index 0 of each subarray   |outputs string representation of integers|        `nil`                              | No                                    |                                       |
+|     3     | method call `first`  |Each sub-array                        |      None                               |  The value at index 0 of each sub-array   | Used to deterine return value of block|
 =end
