196caa2126461766aafd2b42f721a195101e0366
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-11 18:15:59 -0700

    E- hashes, nested iteration

--- rb101/lesson-5/lesson_5.5.rb
+++ rb101/lesson-5/lesson_5.5.rb
@@ -21,3 +21,41 @@ sorted_books = books.sort_by do |book|
               end
 
 p sorted_books
+
+
+# E3 For each of these collections, access the letter 'g'
+
+arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]]
+# p arr1[2][1][3]
+
+arr2 = [{first: ['a', 'b', 'c'], second: ['d', 'e', 'f']}, {third: ['g', 'h', 'i']}]
+# p arr2[1][:third][0]
+
+arr3 = [['abc'], ['def'], {third: ['ghi']}]
+# p arr3[2][:third][0][0]
+
+hsh1 = {'a' => ['d', 'e'], 'b' => ['f', 'g'], 'c' => ['h', 'i']}
+# p hsh1['b'][1]
+
+hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
+# p hsh2[:third].key(0)
+
+# E4 Given this nested hash:
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+# figure out the total age of just the male members of the family
+total_male_age = 0
+munsters.each do |person|
+  person.each do |item|
+    if item.size > 1 && item["gender"] == "male"
+      total_male_age += item["age"]
+    end
+  end
+end
+p total_male_age
+
