703a2e17fd33fda6680eaa588f337956c209e0df
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-13 18:13:19 -0700

    E- step method

---
+++ exercises/rb101-small-problems/documentation/keyword_arguments.rb
@@ -0,0 +1,8 @@
+# what does the code print?
+5.step(to: 10, by: 3) { |value| puts value }
+# SOLUTION
+# 5
+# 8
+# .step method takes the starting position, the calling object in this case 5
+# and steps up in intervals specified by the arguments passed in "by"
+# the argument passed in "to" tells it to stop before getting to that number
