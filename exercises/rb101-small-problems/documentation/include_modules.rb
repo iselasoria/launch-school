47f2ebfdafbafba99889d54191c0c4ce4986938c
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-13 19:05:25 -0700

    E- mix-ins en lieu of multiple inheritance

---
+++ exercises/rb101-small-problems/documentation/include_modules.rb
@@ -0,0 +1,7 @@
+# use irb to run the following code
+a = [5, 9, 3, 11]
+puts a.min
+
+# find the documentation for the #min method and change the above code to
+# print the two smallest values in the Array
+puts a.min(2)
