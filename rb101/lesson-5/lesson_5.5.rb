99afb53f4298d6f3630056e9353504d209a5fd38
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-17 19:24:23 -0700

    E- recreating a hash but with transformation

--- rb101/lesson-5/lesson_5.5.rb
+++ rb101/lesson-5/lesson_5.5.rb
@@ -149,3 +149,21 @@ new_arr = arr.map do |item|
             end
             end
 p new_arr
+
+
+# E10 Given the following data structure, and without modifying the original array, use the 
+# map method to return a new array identical in structure to the original but where the value
+# of each integer is incremented by 1.
+
+arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+# p arr.object_id
+# expected => [{a: 2}, {b: 3, c: 4}, {d: 5, e: 6, f: 7}]
+
+new_arr = arr.map do |hashi|
+            hashi.each_with_object({}) do |(key, value), newhash|
+              newhash[key] = value + 1
+       
+            end
+          end
+p new_arr
+
