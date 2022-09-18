8bdb23421973b7e2dc3ad3fafdacc64976a59c28
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-17 19:00:56 -0700

    E- descending order sort

--- rb101/lesson-5/lesson_5.5.rb
+++ rb101/lesson-5/lesson_5.5.rb
@@ -137,3 +137,15 @@ hsh.each do |_, word_group|
 end
 
 p vowels_in_values.join
+
+# E9 Given this data structure, return a new array of the same structure but with the sub-arrays
+# being ordered (alphabetically or numerically) in descending order.
+
+arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
+
+new_arr = arr.map do |item|
+            item.sort do |a, b|
+              b <=> a
+            end
+            end
+p new_arr
