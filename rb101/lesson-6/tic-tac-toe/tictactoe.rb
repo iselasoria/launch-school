a8f76142942e258d953fd781bead889fa172b93f
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-22 19:35:31 -0700

    P- final walkthrough and rubocop check

--- rb101/lesson-6/tic-tac-toe/tictactoe.rb
+++ rb101/lesson-6/tic-tac-toe/tictactoe.rb
@@ -1,41 +1,44 @@
 require 'pry'
 
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # winning rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # winning cols
+                [[1, 5, 9], [3, 5, 7]] # winning diagonals
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
-
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
-  puts ""
-  puts "     |     |"
+  puts ''
+  puts '     |     |'
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-  puts "     |     |"
-  puts ""
+  puts '     |     |'
+  puts ''
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def initialize_board()
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -45,7 +48,8 @@ def player_places_piece!(brd)
     prompt "Choose a square (#{empty_squares(brd).join(',')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry that is not a valid chice!" # no need for else, this line wont be reached if we break
+
+    prompt 'Sorry that is not a valid chice!' # no need for else, this line wont be reached if we break
   end
   brd[square] = PLAYER_MARKER
 end
@@ -64,33 +68,25 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1,2,3],[4,5,6],[7,8,9]] + # winning rows
-                  [[1,4,7],[2,5,8],[3,6,9]] + # winning cols
-                  [[1,5,9],[3,5,7]]           # winning diagonals
-
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER && 
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER && 
-      brd[line[1]] == COMPUTER_MARKER &&
-      brd[line[2]] == COMPUTER_MARKER
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3 # splat operator equivalent of passing line[0], line[1], line[2]
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
-  nil # outside the each . If inside each, it will return the rinning lines (each returns the caller) which will always be true and break out loop 
+  nil # outside the each . If inside each, it will return the rinning lines (each returns the caller) which will always be true and break out loop
 end
 
 loop do
-
-  board = initialize_board 
-
+  board = initialize_board
 
   loop do
     display_board(board)
 
+    # binding.pry
     player_places_piece!(board)
+    # binding.pry
     break if someone_won?(board) || board_full?(board)
 
     computer_places_piece!(board)
@@ -104,9 +100,9 @@ loop do
   else
     prompt "It's a tie!"
   end
-  prompt "Do you want to play again? (y or n)"
+  prompt 'Do you want to play again? (y or n)'
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thanks for playing Tic-Tac-Toe, good bye!")
+prompt('Thanks for playing Tic-Tac-Toe, good bye!')
