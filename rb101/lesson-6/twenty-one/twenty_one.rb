6ed972ea23c545602ef5d50d967e8f384d45ee11
Rosa <rosaiselasoriamonzon@gmail.com>
2022-09-30 19:40:32 -0700

    P- refining the dealer turn

--- rb101/lesson-6/twenty-one/twenty_one.rb
+++ rb101/lesson-6/twenty-one/twenty_one.rb
@@ -64,57 +64,69 @@ end
 # we initialize these outside the loop because we don't want to reset the hand with each hit
 player_hand = hand!(DECK)
 dealer_hand = hand!(DECK)
+new_card = []
 bust = false
 current_hand = []
 
 loop do
   puts "Your initial hand is: #{player_hand} with a value of: #{calculate_current_hand(player_hand)}"
-  puts "The dealer has #{dealer_hand} and an unkown card"
+  puts "The dealer has #{dealer_hand} and an unkown card" # TODO we need to hide the card here
 
   prompt('Hit or stay?')
   answer = gets.chomp.downcase
 
+  # binding.pry
   if answer == 'hit'
     new_card = hit!(DECK)
     prompt("Your new card is: #{new_card}")
     current_hand = player_hand << new_card
+    # binding.pry
     puts "Your updated hand is now: #{current_hand}"
     puts "Your hand is now worth: #{calculate_current_hand(current_hand)}"
   elsif answer == 'stay'
-    puts "YOU CHOSE TO SAY, NO ON TO THE DEALER"
-
-    system 'clear'
-
-    loop do
-      # break if calculate_current_hand(dealer_hand) >= 17
-      puts "Dealer's hand is: #{dealer_hand}"
-      puts 'Total hand value: #{calculate_current_hand(dealer_hand)}'
-      puts 'Dealer is deciding whether to hit or not...'
-      if calculate_current_hand(dealer_hand) < 17
-        dealer_new_card = hit!(DECK)
-        prompt("Dealer chose to hit, their new card is: #{dealer_new_card}")
-        dealer_current_hand = dealer_hand << dealer_new_card
-        puts "Total updated hand value: #{calculate_current_hand(dealer_current_hand)}"
-      else
-        puts "Dealer chose to stay. Comparing results shortly."
-      end
-    end
+    current_hand = player_hand # this has to exists here in case player decides to never hit
+    puts "You chose to stay, now yielding the turn to the Dealer"
   end
 
   # binding.pry
-  # bust = busted?(calculate_current_hand(current_hand))
-  break if answer == 'stay' || busted?(calculate_current_hand(current_hand))
+  bust = busted?(calculate_current_hand(current_hand)) #calling this method stack in the break condition was not working, why?
+  break if answer == 'stay' || bust
 end
+
+
 
 if busted?(calculate_current_hand(current_hand))
   puts "You busted, the Dealer wins!"
+else
+  loop do
+    puts "Dealer hand: #{dealer_hand}"
+    puts "Dealer is deciding whether to hit or not"
+
+    if calculate_current_hand(dealer_hand) < 17
+      dealer_new_card = hit!(DECK)
+      puts dealer_new_card
+      prompt("Dealer chose to hit, their new card is: #{dealer_new_card}")
+      dealer_current_hand = dealer_hand << dealer_new_card
+      p "The updated hand is now: #{dealer_current_hand}"
+      p calculate_current_hand(dealer_current_hand)
+      puts "Total updated hand value: #{calculate_current_hand(dealer_current_hand)}"
+    else
+      puts "DEALER CHOSE TO STAY"
+    end
+    break
+  end
+
 end
-# if bust # game over if bust
-#   puts 'You busted! The Dealer wins!'
-# else # game continues but goes to the deler's turn
-#   puts 'You chose to stay, now yielding the turn to the Dealer'
-#   loop do
-#     puts "HERE COMES THE DEALER HAND"
-#     break
-#   end
+# puts bust
+# if bust
+#   puts "You busted, the Dealer wins!"
+# else
+#   puts "The dealer's hand is: #{dealer_hand}"
+#   # here we enter the dealer's turn
+#   # loop do
+#   #   puts "IN LOOP Dealer's hand is: #{dealer_hand}"
+#   #   puts "Total value: #{calculate_current_hand(dealer_hand)}"
+#   #   puts 'Dealer is deciding whether to hit or not...'
+#   # end
+
 # end
