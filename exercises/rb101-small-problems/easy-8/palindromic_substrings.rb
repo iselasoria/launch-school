b61e304c43115f9f48a538bb4e3695667ebcbde8
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-05 22:13:38 -0700

    E- problem statement, needs solving

---
+++ exercises/rb101-small-problems/easy-8/palindromic_substrings.rb
@@ -0,0 +1,20 @@
+=begin
+-----------------------INSTRUCTIONS--------------------------------------
+Write a emthod that returns a list of all substrings of a string that are
+palindromic. That is, each substring must consist of the same sequence
+of characters forward as it does backward. The return value should be arranged in
+the same sequence as the substrings appear in the string. Duplicate palindromes should be
+included multiple times.
+
+--------------------------PROBLEM----------------------------------------
+Questions:
+Input: 
+Output: 
+---------------------------RULES-----------------------------------------
+Explicit: 
+Implicit: 
+--------------------------EXAMPLES---------------------------------------
+----------------------------ALGO----------------------------------------
+=end
+
+# TODO
