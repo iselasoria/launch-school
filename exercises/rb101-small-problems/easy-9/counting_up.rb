47c807eeb2ac5c0999a3b008279a5392d17e9bb5
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-07 21:46:47 -0700

    E- from 1 to input array

---
+++ exercises/rb101-small-problems/easy-9/counting_up.rb
@@ -0,0 +1,30 @@
+=begin
+-----------------------INSTRUCTIONS--------------------------------------
+Wrirte a methid tghat takes anb integer argument, and returns an array of all
+integers, in sequence, between 1 and the argument. You may assume that the 
+input will always be a valid positive integer.
+--------------------------PROBLEM----------------------------------------
+Questions:
+Input: 
+- integer
+Output: 
+- array
+---------------------------RULES-----------------------------------------
+Explicit: 
+- the array must range between 1 and the input integer, in increments of 1
+Implicit: 
+--------------------------EXAMPLES---------------------------------------
+----------------------------ALGO----------------------------------------
+- create a range of integers
+- return the range
+=end
+
+
+def sequence(num)
+  arre = (1..num).to_a
+
+end
+
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]
