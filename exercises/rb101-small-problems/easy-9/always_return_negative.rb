b91098e229f60aa88b3b42181f59adb1676d2f68
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-07 21:53:27 -0700

    E- adding the done tag

--- exercises/rb101-small-problems/easy-9/always_return_negative.rb
+++ exercises/rb101-small-problems/easy-9/always_return_negative.rb
@@ -33,3 +33,5 @@ end
 p negative(5) == -5
 p negative(-3) == -3
 p negative(0) == 0      # There's no such thing as -0 in ruby
+
+# * DONE
--- exercises/rb101-small-problems/easy-9/counting_up.rb
+++ exercises/rb101-small-problems/easy-9/counting_up.rb
@@ -28,3 +28,5 @@ end
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
 p sequence(1) == [1]
+
+# * DONE
