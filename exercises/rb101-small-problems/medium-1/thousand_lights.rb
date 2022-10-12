fbeadd368d699acbbf7662f24d6f85c31dc311a5
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-11 18:21:55 -0700

    E- one thousand lights

---
+++ exercises/rb101-small-problems/medium-1/thousand_lights.rb
@@ -0,0 +1,33 @@
+=begin
+-----------------------INSTRUCTIONS--------------------------------------
+You have a bank of switches before you, numbered 1 to `n`. Each switch is connected to exactly one light that is initially off.
+You walk by the row of switches and toggle every one of them on. You go back to the beginning, and on this second pass, you
+toggle switches 2,4,6, and so on. On the third pass, you go back again to the beginning and toggle switches 3,6,9 and so on.
+You repeat this process and keep going until you have been through `n` repetitions.
+
+Write a method that takes one argument, the total number of switches, and returns an aray that identifies which
+lights are on after `n` repetitions.
+
+--------------------------PROBLEM----------------------------------------
+Questions:
+Input: integer
+Output: array of integers
+---------------------------RULES-----------------------------------------
+Explicit: 
+- with each pass, the multiples of the current itration get toggled
+Implicit: 
+- the ones that are not multiples do not get changed
+--------------------------EXAMPLES---------------------------------------
+      |   1   |   2   |   3   |   4   |   5   |
+      +-------+-------+-------+-------+-------+
+Rep 1 |   *   |   *   |   *   |   *   |   *   |
+Rep 2 |   *   |       |   *   |       |   *   |
+Rep 3 |   *   |       |       |       |   *   |
+Rep 4 |   *   |       |       |   *   |   *   |
+Rep 5 |   *   |       |       |   *   |       |
+
+result = [1,4] are the only lights that are on at the end of `n` repetitions.
+
+
+----------------------------ALGO----------------------------------------
+=end
