c437f71c13213b79c98fd3de02a55ed68887d00a
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-16 16:36:25 -0700

    E- odd nums

---
+++ exercises/rb101-small-problems/second-pass/easy-2/odd_numbers.rb
@@ -0,0 +1,23 @@
+=begin
+-----------------------INSTRUCTIONS--------------------------------------
+Print all off numbers from 1 to 99, inlcusive with each on a separate line.
+--------------------------PROBLEM----------------------------------------
+Questions:
+Input: 
+Output: 
+---------------------------RULES-----------------------------------------
+Explicit: 
+Implicit: 
+--------------------------EXAMPLES---------------------------------------
+----------------------------ALGO----------------------------------------
+=end
+
+arr = (1..99)
+
+arr.each do |num|
+  if num.odd?
+    puts num
+  end
+end
+
+# DONE
