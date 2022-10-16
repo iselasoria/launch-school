88c1519f7146597a4df5bfa80782aab8e517d577
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-15 23:25:17 -0700

    E- reverse part 1

--- exercises/rb101-small-problems/second-pass/easy-1/reverse-pt1.rb
+++ exercises/rb101-small-problems/second-pass/easy-1/reverse-pt1.rb
@@ -25,3 +25,5 @@ puts reverse_sentence('Hello World') == 'World Hello'
 puts reverse_sentence('Reverse these words') == 'words these Reverse'
 puts reverse_sentence('') == ''
 puts reverse_sentence('    ') == '' # Any number of spaces results in ''
+
+# DONE
