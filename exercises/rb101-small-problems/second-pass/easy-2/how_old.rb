5e8ddf2aa220833f32a2dc8665e0d6003de9c60a
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-16 16:02:20 -0700

    E- how old is teddy, Kernel#rand

---
+++ exercises/rb101-small-problems/second-pass/easy-2/how_old.rb
@@ -0,0 +1,19 @@
+=begin
+-----------------------INSTRUCTIONS--------------------------------------
+Build a program that randomly generates and prints Teddy's age. To get the age,
+you should generate a random number between 20 and 200.
+--------------------------PROBLEM----------------------------------------
+Questions:
+Input: 
+Output: 
+---------------------------RULES-----------------------------------------
+Explicit: 
+Implicit: 
+--------------------------EXAMPLES---------------------------------------
+----------------------------ALGO----------------------------------------
+=end
+
+age = rand(20..200)
+puts "Teddy is #{age} years old!"
+
+# DONE
