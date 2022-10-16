e0ccffbb7b8b48c02ebc26e657ed621503f9fbb8
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-15 23:03:15 -0700

    E-absolute value

---
+++ exercises/rb101-small-problems/second-pass/easy-1/odd.rb
@@ -0,0 +1,41 @@
+=begin
+-----------------------INSTRUCTIONS--------------------------------------
+Write a methoid that takes one integer argument, which may be positive, negative, or zero.
+This method returns `true` if the numbers absolute value is idd. You may assume that
+the argument is a valid integer value.
+--------------------------PROBLEM----------------------------------------
+Questions:
+Input: 
+- integer +, -, or 0
+Output: boolean
+---------------------------RULES-----------------------------------------
+Explicit: 
+- if absolute value of number is odd, method returns true,
+- if the absolutel value is even, it returns false
+- if it is 0, the method will also return false
+Implicit: 
+--------------------------EXAMPLES---------------------------------------
+----------------------------ALGO----------------------------------------
+- read the ineger
+- store the absolute value in a variable
+  - if the number is negative, multiply it by -1 to get a positive (its absolute value)
+  - if the number is positive, just store it directly 
+- if the number is odd? return true if not, return false
+=end
+
+
+def is_odd?(num)
+  number = num < 1 ? num * -1 : num
+
+  number.odd? ? true : false
+
+end
+
+
+# test cases
+puts is_odd?(2) == false   # => false
+puts is_odd?(5)  ==true   # => true
+puts is_odd?(-17) == true  # => true
+puts is_odd?(-8) == false  # => false
+puts is_odd?(0) == false   # => false
+puts is_odd?(7)  ==true  # => true
