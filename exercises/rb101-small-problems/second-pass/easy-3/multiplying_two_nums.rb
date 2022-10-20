30f3a187cc8f340f3a7832e90408c6907a737580
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-20 14:00:09 -0700

    E- multipltication

---
+++ exercises/rb101-small-problems/second-pass/easy-3/multiplying_two_nums.rb
@@ -0,0 +1,10 @@
+=begin
+Create a method that takes two arguments, multiplies them together and returns the result.
+=end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+# test case
+p multiply(5, 3) == 15
