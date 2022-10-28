c62866d4dfe06c4dd6b644139974e6c00e8fe2c7
Rosa <rosaiselasoriamonzon@gmail.com>
2022-10-27 17:43:40 -0700

    E- star problem

---
+++ exercises/rb101-small-problems/advanced-1/seeing_stars.rb
@@ -0,0 +1,7 @@
+=begin
+Write a method that displays an 8-pointed star in an nxn grid,
+where n is an odd integer that is supplied as an argument to the method.
+The smallest such star you need to handle is a 7x7 grid.
+
+=end 
+# todo come back to this after algo
