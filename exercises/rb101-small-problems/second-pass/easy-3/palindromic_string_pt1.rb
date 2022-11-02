8a0058ea9ac15025b6d1985c41d2dc5c42246993
Rosa <rosaiselasoriamonzon@gmail.com>
2022-11-01 17:06:30 -0700

    E- palindromic 1

--- exercises/rb101-small-problems/second-pass/easy-3/palindromic_string_pt1.rb
+++ exercises/rb101-small-problems/second-pass/easy-3/palindromic_string_pt1.rb
@@ -41,13 +41,13 @@ a.chars == b.chars.reverse
 
 
 def palindrome?(str)
-  str = str.reverse ? true : false
+  str == str.reverse ? true : false
 end
 
 # test cases
-p palindrome?('madam') #== true
-p palindrome?('Madam') #== false          # (case matters)
-p palindrome?("madam i'm adam") #== false # (all characters matter)
+p palindrome?('madam') == true
+p palindrome?('Madam') == false          # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
 p palindrome?('356653') == true
 
 # DONE
