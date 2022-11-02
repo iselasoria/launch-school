27627b8b5c85ffc6c69719bc3769191a0e00e961
Rosa <rosaiselasoriamonzon@gmail.com>
2022-11-01 17:05:38 -0700

    E- palindromic 2

--- exercises/rb101-small-problems/second-pass/easy-3/palindromic_string_pt2.rb
+++ exercises/rb101-small-problems/second-pass/easy-3/palindromic_string_pt2.rb
@@ -3,7 +3,37 @@ Write another method that returns true if the string passed as an argument is a 
 false otherwise. This time, however, your method should be case-insensitive, and it should
 ignore all non-alphanumeric characters. If you wish, you may simplify things by calling
 the palindrome? method you wrote in the previous exercise.
+
+
+------------------------------------E
+'356a653'
+ 356
+    a
+     653
+ ---> the method finds every character in the same order in either direction
+
+'123ab321'
+ 123
+    a      -----|
+     b     -----|
+      321
+  --------> here the method finds a b when it goes backward and an a when going forward, so not a plaindrome
+
+------------------------------------A
+- clean the string 
+  - downcasing all 
+  - deleting anything that is not a letter or a number
+
 =end
+
+def palindrome?(str)
+  str == str.reverse ? true : false
+end
+
+def real_palindrome?(string)
+  string = string.downcase.delete('^a-z0-9')
+  palindrome?(string)
+end
 
 # test cases
 p real_palindrome?('madam') == true
