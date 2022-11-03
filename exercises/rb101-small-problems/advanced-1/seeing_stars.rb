69709ffd13bab54c6315190f740db527e49fd2c7
Rosa <rosaiselasoriamonzon@gmail.com>
2022-11-02 18:47:24 -0700

    E- seeing stars

--- exercises/rb101-small-problems/advanced-1/seeing_stars.rb
+++ exercises/rb101-small-problems/advanced-1/seeing_stars.rb
@@ -3,6 +3,32 @@ Write a method that displays an 8-pointed star in an nxn grid,
 where n is an odd integer that is supplied as an argument to the method.
 The smallest such star you need to handle is a 7x7 grid.
 
+
+input: integer 
+output: a series of strings
+
+Goal: use the input integer as the size of the grid
+
+-----------------------E
+Model Solution:
+when input is 7 the grid looks like this:
+  |||||||
+  |||||||
+  |||||||
+  |||||||
+  |||||||
+  |||||||
+  |||||||
+             limit is 7 on all of them
+  *||*||* star space space star space space star ----> space: 4 star: 3
+  |*|*|*| space star space star space star space ----> space: 4 star: 3
+  ||***|| space space star star star space space ----> space: 4 star: 3
+  ******* star  star  star star star star  star  ----> space: 0 star: 7
+  ||***|| space space star star star space space ----> space: 4 star: 3
+  |*|*|*| space star space star space star space ----> space: 4 star: 3
+  *||*||* star space space star space space star ----> space: 4 star: 3
+
+
 =end 
 # todo come back to this after algo
 
