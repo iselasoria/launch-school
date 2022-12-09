531af197131a823f2fd1c8d3523ab425ffd115b6
Rosa <rosaiselasoriamonzon@gmail.com>
2022-12-08 16:20:47 -0800

    E- matching parenthesis

--- exercises/rb101-small-problems/medium-2/matching_parentheses.rb
+++ exercises/rb101-small-problems/medium-2/matching_parentheses.rb
@@ -98,22 +98,22 @@ p balanced?('What ((is))) up(') == false # if all the parentheses that have been
 # This way when a string has a zero parenthesis count, we know the string is balanced. Much like a zero-based budget. We can use a break in the
 # iteration to exit if we find that an opening parenthesis comes first-- the parenthesis count is a negative number.
 
-def balanced?(string)
-  parens = 0
-  string.each_char do |char|
-    parens += 1 if char == '('
-    parens -= 1 if char == ')'
-    break if parens < 0
-  end
+# def balanced?(string)
+#   parens = 0
+#   string.each_char do |char|
+#     parens += 1 if char == '('
+#     parens -= 1 if char == ')'
+#     break if parens < 0
+#   end
 
-  parens.zero?
-end
+#   parens.zero?
+# end
 
-p balanced?('What (is) this?') == true
-p balanced?('What is) this?') == false
-p balanced?('What (is this?') == false
-p balanced?('((What) (is this))?') == true
-p balanced?('((What)) (is this))?') == false
-p balanced?('Hey!') == true
-p balanced?(')Hey!(') == false
-p balanced?('What ((is))) up(') == false
+# p balanced?('What (is) this?') == true
+# p balanced?('What is) this?') == false
+# p balanced?('What (is this?') == false
+# p balanced?('((What) (is this))?') == true
+# p balanced?('((What)) (is this))?') == false
+# p balanced?('Hey!') == true
+# p balanced?(')Hey!(') == false
+# p balanced?('What ((is))) up(') == false
