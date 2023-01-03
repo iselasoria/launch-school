3127ec8f1ab103306d5fe8e1e605d24730b655fc
Rosa <rosaiselasoriamonzon@gmail.com>
2023-01-03 15:22:06 -0800

    MP- E1 repeat

--- exercises/mastery-practice/ls-exercises/easy-1/repeat_yourself.rb
+++ exercises/mastery-practice/ls-exercises/easy-1/repeat_yourself.rb
@@ -1,2 +1,11 @@
-# Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+# Write a method that takes two arguments, a string and a positive integer, 
+# and prints the string as many times as the integer indicates.
+
+def repeat(str, num)
+  num.times do |word| 
+    puts str
+  end
+end
+
+
 repeat('Hello', 3)
