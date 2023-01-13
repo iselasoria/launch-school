421e55f1faf90b4f7b48f05946a8cc520cc89789
Rosa <rosaiselasoriamonzon@gmail.com>
2023-01-12 19:58:47 -0800

    MP- E3 multiply

---
+++ exercises/mastery-practice/ls-exercises/easy-3/multiply_two_nums.rb
@@ -0,0 +1,10 @@
+=begin 
+* DONE 1m1s
+Create a method that takes two arguments, multiplies them together, and returns the result.
+=end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+p multiply(5, 3) == 15
