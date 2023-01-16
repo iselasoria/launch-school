06cd0ebcf03848e80cf43d5411d9e9234963dddd
Rosa <rosaiselasoriamonzon@gmail.com>
2023-01-15 16:47:27 -0800

    MP- chunking

--- exercises/mastery-practice/chunking/mathematics-algorithms.rb
+++ exercises/mastery-practice/chunking/mathematics-algorithms.rb
@@ -1,4 +1,20 @@
 ################################################################################################
+# ! Letter Counter
+# * English Breakdown
+# * Ruby Breakdown
+
+def word_sizes(str)
+  counts = Hash.new(0)
+
+  str.split.each do |word|
+    counts[word.size] += 1
+  end
+
+  counts
+end # todo
+
+
+################################################################################################
 # ! Name of Algorithm
 # * English Breakdown
 # * Ruby Breakdown
