9cf4e70beff28425b3b4bc4f55045d186d9101c6
Rosa <rosaiselasoriamonzon@gmail.com>
2023-01-19 14:27:04 -0800

    MP- include, return to this

---
+++ exercises/mastery-practice/ls-exercises/easy-6/include.rb
@@ -0,0 +1,22 @@
+=begin 
+Write a method named include? that takes an Array and a search 
+value as arguments. This method should return true if the 
+search value is in the array, false if it is not. You may not 
+use the Array#include? method in your solution.
+
+=end
+
+def include?(arr, term)
+  return true if (term.nil? && arr.all? (nil))
+  
+  search = arr.find {|item| item == term}
+
+  search ? true : false
+end
+
+# test cases 
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
