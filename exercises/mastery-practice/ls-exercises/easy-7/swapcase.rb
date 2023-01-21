60b99d02d45bd973eafdabf066fa41d985e583e5
Rosa <rosaiselasoriamonzon@gmail.com>
2023-01-20 21:07:55 -0800

    MP- e7 interview prep fixed

--- exercises/mastery-practice/ls-exercises/easy-7/swapcase.rb
+++ exercises/mastery-practice/ls-exercises/easy-7/swapcase.rb
@@ -1,5 +1,7 @@
 =begin 
 * DONE 5m
+ TODO Interview Practice
+ 
 Write a method that takes a string as an argument and returns a new string in which every uppercase letter is replaced by its lowercase version, and every lowercase letter by its uppercase version. All other characters should be unchanged.
 
 You may not use String#swapcase; write your own version of this method.
