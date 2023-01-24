ff007427149bdf703220f35ecc17090f206911fa
Rosa <rosaiselasoriamonzon@gmail.com>
2023-01-23 18:46:49 -0800

    MP- E7 include method from scratch

---
+++ exercises/mastery-practice/ls-exercises/easy-7/include.rb
@@ -0,0 +1,23 @@
+=begin 
+* DONE 1m37s
+Write a method named include? that takes an Array and a search value as arguments. 
+This method should return true if the search value is in the array, false if it is not. 
+You may not use the Array#include? method in your solution.
+
+Algorithm: 
+- iterate over the array 
+  - if the current item is equal to the search, return true 
+=end
+
+def include?(arr, search)
+  arr.any? do |item|
+    item == search
+  end
+end
+
+# test cases 
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
