1d862c0a9d5b44837d43f538472823c2649ebe65
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-15 14:53:15 -0700

    MP- RB101 Final Commit

---
+++ exercises/mastery-practice/interview_stats.rb
@@ -0,0 +1,189 @@
+codewars/ls-list/46_digital_root.rb:2:# * DONE 9m45s
+codewars/ls-list/51_revrot.rb:2:* DONE 52m26s
+codewars/ls-list/55_counting_grouping.rb:1:# * DONE 15m2s
+codewars/ls-list/mexican_wave.rb:2:# * DONE 19m
+codewars/rs-list/TA_session_insert_missing.rb:2:# * DONE 22m
+codewars/rs-list/encrypt_decrypt.rb:2:* DONE 21m---> 50m
+codewars/rs-list/hash_to_str.rb:2:# * DONE 17m
+codewars/rs-list/jackpot.rb:2:* DONE 20m17s
+codewars/rs-list/sample_ta.rb:1:# * DONE 40m
+codewars/rs-list/steps_in_primes.rb:2:* DONE 18m19s
+codewars/rs-list/subs_consonants.rb:2:* DONE 25m
+codewars/rs-list/valid_words_in_arr.rb:1:# * DONE 11 m
+codewars/rs-list/your_order_please.rb:2:* DONE 20m 
+edabit/bigrams.rb:2:* DONE 18m04s
+edabit/cipher_me.rb:2:* DONE 18m18s
+edabit/closest_vowel.rb:2:* DONE 21
+edabit/counting_boomerangs.rb:2:# DONE 18m19s
+edabit/digital_vowel_ban.rb:2:* DONE 33m15s
+edabit/edit_words.rb:2:* DONE 18m04s
+edabit/fib_str.rb:2:* DONE 18m21s
+edabit/files_search.rb:2:* DONE 36m19s
+edabit/find_occurences.rb:2:* DONE 09m40s
+edabit/first_repeat.rb:2:* DONE 7m18s
+edabit/folded.rb:2:* DONE 15m42s
+edabit/generate_palindromes.rb:2:* DONE 15m
+edabit/get_sentence_value.rb:2:* DONE 19m42s
+edabit/group_vowels.rb:2:* DONE 22m00s
+edabit/index_of_caps.rb:3:* DONE 14m10s
+edabit/inverter.rb:2:* DONE 10m13s
+edabit/is_alpha.rb:2:* DONE 3m
+edabit/is_smooth.rb:2:* DONE 21m48s
+edabit/longest_non_rep.rb:2:* DONE 09m00s
+edabit/palindrome_set.rb:2:* DONE 41m00s
+edabit/scrabble.rb:2:* DONE 13m50s
+edabit/shadow_sentnence.rb:2:* DONE 21m09s
+edabit/slice_array.rb:3:* DONE 07m26s
+edabit/sliding_sum.rb:2:* DONE 09m59s
+edabit/spiral_benji.rb:1:# * DONE 1558s
+edabit/substring_test_II.rb:2:* DONE 13m30s
+edabit/tesh_is_my_world.rb:2:* DONE 09m04s
+edabit/word_weights.rb:2:* DONE 15m46s
+interview-round-2/III_closest_numbers.rb:2:* DONE 8m
+interview-round-2/III_find_smaller_than_current.rb:1:# * DONE 4m06s
+interview-round-2/III_minimum_sum.rb:1:# * DONE 5m54s
+interview-round-2/III_to_weird_case.rb:1:# * DONE 14m36s
+interview-round-2/II_balaced_parenthesis.rb:2:* DONE 14m18s
+interview-round-2/II_lettercase_pct.rb:2:* DONE 23m30s
+interview-round-2/II_longest_sentence.rb:2:* DONE 15m00s
+interview-round-2/II_now_i_know_abc.rb:2:* DONE 15m45s
+interview-round-2/II_sumsquares_squaresums.rb:2:* DONE 15m21s
+interview-round-2/II_triangle_sides.rb:2:* DONE 25m
+interview-round-2/IV_divisible_sum_pairs.rb:2:* DONE 12m00s
+interview-round-2/IV_dubstep.rb:2:* DONE 4m09s
+interview-round-2/IV_find_even_index.rb:2:* DONE 11m40s
+interview-round-2/IV_find_pairs.rb:2:* DONE 02m00s
+interview-round-2/IV_grouping_counting.rb:2:* DONE 17m40s
+interview-round-2/IV_groups_of_10s.rb:2:* DONE 39m
+interview-round-2/IV_largest_prod_in_series.rb:2:* DONE 11m35s
+interview-round-2/IV_longest_alphabetical_subs.rb:2:* DONE 5m19s
+interview-round-2/IV_longest_vowel_chain.rb:2:* DONE 11m35s new way | old way: 03m07s
+interview-round-2/IV_longest_vowel_chain.rb:37:# old way * DONE 03m07s
+interview-round-2/IV_mexican_Wave.rb:2:* DONE 6m58s
+interview-round-2/IV_nexus.rb:2:* DONE 17m
+interview-round-2/IV_numerals_as_backspace_keys.rb:2:* DONE 04m13s
+interview-round-2/IV_parity_outlier.rb:2:* DONE 5m
+interview-round-2/IV_pete_the_baker.rb:2:* DONE 23m06s
+interview-round-2/IV_playing_with_numbers.rb:2:* DONE 26m30s
+interview-round-2/IV_power.rb:2:* DONE 13m48s
+interview-round-2/IV_rep_substrings.rb:2:* DONE 04m08s
+interview-round-2/IV_replace_with_alpha_position.rb:2:* DONE 10m34s
+interview-round-2/IV_rev_and_combine.rb:2:* DONE 17m29s
+interview-round-2/IV_revrot.rb:2:* DONE 34m39s
+interview-round-2/IV_sherlock_pockets.rb:2:* DONE 8m57s
+interview-round-2/IV_stop_spinning_my_words.rb:2:* DONE 3m0s
+interview-round-2/IV_tripple_double.rb:3:* DONE 09m20s
+interview-round-2/IV_weirdcase.rb:2:* DONE 07m37s
+interview-round-2/IV_wheres_my_parent.rb:2:* DONE 9m0s
+interview-round-2/IV_wheres_my_parent.rb:3:* DONE 2m10s second approach
+interview-round-2/I_diamonds.rb:2:* DONE 29m47s
+interview-round-2/I_diamonds.rb:71:# * DONE 3m36s
+interview-round-2/I_fibonacci.rb:2:# * DONE 5m0s
+interview-round-2/I_mil_luces.rb:2:* DONE 24m24s
+interview-round-2/I_next_featured.rb:2:* DONE 15m41s
+interview-round-2/I_rotation1.rb:2:* DONE 3m17s
+interview-round-2/I_word_to_digit.rb:2:* DONE 15m30s 
+interview-round-2/VI_common_prefix.rb:2:* DONE 16m37s
+interview-round-2/VI_max_sequence.rb:2:* DONE 02m21s
+interview-round-2/VI_next_biggest.rb:2:* DONE 56m29s 
+interview-round-2/VI_next_biggest.rb:3:* DONE 11m43s
+interview-round-2/VI_throwing_darts.rb:2:* DONE 07m04s
+interview-round-2/V_decipher_this.rb:2:* DONE 27m16s
+interview-round-2/V_inside_out_strings.rb:1:# * DONE 21m36s
+interview-round-2/V_sandwiched_string.rb:2:# * DONE 15m27s (during session) 23m48s (Including debugging)
+interview-round-2/V_string_avg.rb:2:* DONE 27m
+interview-round-2/V_three_sum_closest.rb:2:* DONE 16m12s
+interview-round-2/a_chunking.rb:1:# generate subs that are only vowels # DONE 02m40s
+interview-round-2/a_chunking.rb:14:# generate subs that are only consonants # DONE 01m49s
+interview-round-2/a_chunking.rb:27:# generate pairs # DONE 02m50s
+interview-round-2/a_chunking.rb:61:def gen_subs(str) # DONE 07m11s
+interview-round-2/a_chunking.rb:83:# check if a number is prime # DONE 2m30s
+interview-round-2/a_chunking.rb:95:# get a list of all prime up to a certain number # DONE 1m40s
+interview-round-2/a_chunking.rb:104:# get the next n number of primes starting at a given number # DONE 1m50s
+interview-round-2/average_asccii.rb:2:* DONE 15m28s
+interview-round-2/goldbach.rb:2:* DONE 24m19s
+interview-round-2/length_of_arr_size.rb:1:# * DONE 13m20s
+interview-round-2/prime_perms.rb:2:* DONE 34m00s
+interview-round-2/scrambles_woc.rb:2:* DONE 24m05s
+interview-round-2/transform_to_prime.rb:2:* DONE 10m09s
+leet-code/leetcode-75/level-1/find_pivot_index.rb:3:* DONE 13m31s
+leet-code/leetcode-75/level-1/running_sum.rb:2:* DONE 10m
+leet-code/leetcode-75/level-1/subsequence.rb:2:* DONE 15m
+leet-code/selected-problems/rings.rb:2:* DONE 25m49
+ls-exercises/easy-1/get_miggle.rb:2:* DONE 18m36s
+ls-exercises/easy-1/reverse_digits.rb:2:* DONE 3m
+ls-exercises/easy-1/right_triangles.rb:65:# * DONE 4m15s
+ls-exercises/easy-2/always_negative.rb:2:* DONE 1m37s
+ls-exercises/easy-2/even_numbers.rb:3:* DONE 59s
+ls-exercises/easy-2/greeting_users.rb:2:* DONE 6m
+ls-exercises/easy-2/how_big.rb:2:* DONE 4m
+ls-exercises/easy-2/odd_numbers.rb:2:* DONE 1m
+ls-exercises/easy-2/retirement.rb:2:* DONE 3m
+ls-exercises/easy-2/string_assignment.rb:2:* DONE 2m31s
+ls-exercises/easy-2/sum_or_prod.rb:2:* DONE 6m25s
+ls-exercises/easy-2/teddy.rb:2:* DONE 1m07s
+ls-exercises/easy-2/tip_calc.rb:2:* DONE 3m38s
+ls-exercises/easy-3/arithmetic_int.rb:2:* DONE 2m49s
+ls-exercises/easy-3/counting_num_chars.rb:2:* DONE 57s
+ls-exercises/easy-3/multiply_two_nums.rb:2:* DONE 1m1s
+ls-exercises/easy-3/palindromic_numbers.rb:2:* DONE 1m48s
+ls-exercises/easy-3/palindromic_substrings.rb:2:* DONE 50s
+ls-exercises/easy-3/palindromic_substrings_ii.rb:2:* DONE 5m16s
+ls-exercises/easy-3/searching_101.rb:2:* DONE 3m14s
+ls-exercises/easy-3/squaring.rb:2:* DONE 1m
+ls-exercises/easy-3/uppercase_check.rb:2:* DONE 1m56s
+ls-exercises/easy-3/xor.rb:2:* DONE 1m53s
+ls-exercises/easy-4/convert_num_to_str.rb:2:* DONE 2m44s
+ls-exercises/easy-4/convert_string_to_num.rb:2:* DONE 8m55s
+ls-exercises/easy-4/convert_string_to_signed.rb:2:* DONE 12m1s
+ls-exercises/easy-4/leap_years.rb:2:* DONE 18m47s
+ls-exercises/easy-4/leap_years_ii.rb:2:* DONE 3m
+ls-exercises/easy-4/multiples_of_3and5.rb:2:* DONE 18m
+ls-exercises/easy-4/num_to_signed_str.rb:2:* DONE 3m
+ls-exercises/easy-4/running_total.rb:2:* DONE 1m
+ls-exercises/easy-4/short_long_short.rb:2:* DONE 4m
+ls-exercises/easy-5/after_midnights.rb:2:* DONE 21m39s
+ls-exercises/easy-5/aftermidnight_pt2.rb:2:* DONE 20m
+ls-exercises/easy-5/alphabetical_number.rb:2:* DONE 18m41s 
+ls-exercises/easy-5/ascii_string_val.rb:2:* DONE 56s
+ls-exercises/easy-5/cleanup_words.rb:2:* DONE 21m 
+ls-exercises/easy-5/daily_double.rb:2:* DONE 12m25s
+ls-exercises/easy-5/letter_counter_pt1.rb:2:* DONE 12m
+ls-exercises/easy-5/letter_counter_pt2.rb:2:* DONE
+ls-exercises/easy-5/letter_swap.rb:2:* DONE 4m13s
+ls-exercises/easy-5/list_of_digits.rb:2:* DONE
+ls-exercises/easy-5/spin_me_around.rb:2:* DONE 1m
+ls-exercises/easy-6/combining_arrays.rb:2:* DONE 2m58s
+ls-exercises/easy-6/cute_angles.rb:2:* DONE 20m 
+ls-exercises/easy-6/delete_vowels.rb:2:* DONE 6m
+ls-exercises/easy-6/find_dupe.rb:2:* DONE 10m29s
+ls-exercises/easy-6/halvsies.rb:2:* DONE 15m51s
+ls-exercises/easy-6/include.rb:3:* DONE 1m37s
+ls-exercises/easy-7/combine_two_lists.rb:2:* DONE 1m54s
+ls-exercises/easy-7/end_is_near.rb:2:* DONE 34s
+ls-exercises/easy-7/how_many.rb:2:* DONE 2m
+ls-exercises/easy-7/multiplicative_avg.rb:2:* DONE 3m
+ls-exercises/easy-7/multiply_lists.rb:2:* DONE 2m38s
+ls-exercises/easy-7/staggered_caps.rb:2:* DONE 
+ls-exercises/easy-7/swapcase.rb:2:* DONE 5m
+ls-exercises/easy-8/all_substrings.rb:2:* DONE 55s
+ls-exercises/easy-8/array_avg.rb:2:* DONE 1m
+ls-exercises/easy-8/double_char_pt1.rb:2:* DONE 1m
+ls-exercises/easy-8/double_chars_pt.rb:2:* DONE 7m
+ls-exercises/easy-8/fizzbuzz.rb:2:* DONE 3m40s
+ls-exercises/easy-8/growing_substrings.rb:2:* DONE 2m
+ls-exercises/easy-8/palindromic_substrings.rb:2:* DONE 5m20s
+ls-exercises/easy-8/reverse_pt1.rb:2:* DONE 2m
+ls-exercises/easy-8/reverse_pt2.rb:2:* DONE 7m36s
+ls-exercises/easy-8/sum_of_sums.rb:2:* DONE 6m
+ls-exercises/easy-9/counting_up.rb:2:* DONE 1m
+ls-exercises/easy-9/double_double.rb:2:* DONE 15m
+ls-exercises/easy-9/gradebook.rb:2:* DONE 4m26
+ls-exercises/easy-9/grocery_list.rb:2:* DONE
+ls-exercises/easy-9/group_anagrams.rb:1:# *DONE 5m
+ls-exercises/easy-9/how_long_are_you.rb:2:* DONE 3m
+ls-exercises/easy-9/sequence_count.rb:2:* DONE 
+ls-exercises/easy-9/welcom_stranger.rb:2:* DONE 4m
+ls-exercises/interview-prep-problems/nums_smaller_than_current.rb:1:# * DONE 14m37s
+ls-exercises/medium-1/rotate_1.rb:2:* DONE 5m
+ls-exercises/medium-1/rotate_2.rb:2:* DONE 10m
