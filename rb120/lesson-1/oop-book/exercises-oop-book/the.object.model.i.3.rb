dd5ab417ba2e8c4bcdef46f62e53df62159a857b
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-16 20:45:01 -0700

    dir structure cleanup

---
+++ rb120/lesson-1/oop-book/exercises-oop-book/the.object.model.i.3.rb
@@ -0,0 +1,5 @@
+=begin 
+
+3. You want to create a nice interface that allows you to accurately describe the action you want your program 
+to perform. Create a method called spray_paint that can be called on an object and will modify the color of the car.
+=end 
