8234c6874be8f24cb9667d4400f2f5693895a46d
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-17 19:10:05 -0700

    E- oop exercises redo

--- rb120/lesson-1/oop-book/exercises-oop-book/classes_and_objects_i_2.rb
+++ rb120/lesson-1/oop-book/exercises-oop-book/classes_and_objects_i_2.rb
@@ -1,4 +1,39 @@
 =begin 
-2. Add an accessor method to your MyCar class to change and view the color of your car. Then add an accessor 
-method that allows you to view, but not modify, the year of your car.
+2. Add an accessor method to your MyCar class to change and view the color of your car. 
+Then add an accessor method that allows you to view, but not modify, the year of your car.
 =end 
+
+class MyCar
+  attr_accessor :color 
+  attr_reader :year 
+
+  def initialize(year, color, model)
+    @current_speed = 0
+    @year = year 
+    @color = color 
+    @model = model 
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You pushed the gas pedal and accelerated #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You pushed the break and decelerated #{number}."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_off
+    @current_speed = 0 
+    puts "Let's park!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'red')
+puts lumina.color = 'blue'
+puts lumina.year
--- rb120/lesson-1/oop-book/exercises-oop-book/classes_and_objects_i_3.rb
+++ rb120/lesson-1/oop-book/exercises-oop-book/classes_and_objects_i_3.rb
@@ -1,5 +1,45 @@
 =begin 
 
-3. You want to create a nice interface that allows you to accurately describe the action you want your program 
-to perform. Create a method called spray_paint that can be called on an object and will modify the color of the car.
+3. You want to create a nice interface that allows you to accurately describe 
+the action you want your program to perform. Create a method called spray_paint
+that can be called on an object and will modify the color of the car.
 =end 
+
+class MyCar
+  attr_accessor :color 
+  attr_reader :year 
+
+  def initialize(year, color, model)
+    @current_speed = 0
+    @year = year 
+    @color = color 
+    @model = model 
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You pushed the gas pedal and accelerated #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You pushed the break and decelerated #{number}."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_off
+    @current_speed = 0 
+    puts "Let's park!"
+  end
+
+  def spray_paint(color)
+    self.color= color
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'red')
+lumina.spray_paint('pink')
