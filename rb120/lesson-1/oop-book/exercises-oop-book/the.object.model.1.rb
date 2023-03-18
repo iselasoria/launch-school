bc48266d06939c7ac130e3fa58d6c0648acd1efb
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-17 18:46:28 -0700

    E- exercises on the OOP book

---
+++ rb120/lesson-1/oop-book/exercises-oop-book/the.object.model.1.rb
@@ -0,0 +1,9 @@
+=begin 
+How do we create an object in Ruby? Give an example of the creation of an object.
+=end 
+
+def Animal
+
+end
+
+my_cat = Animal.new
---
+++ rb120/lesson-1/oop-book/exercises-oop-book/the.object.model.2.rb
@@ -0,0 +1,19 @@
+=begin 
+What is a module? What is its purpose? How do we use them with our classes? 
+Create a module for the class you created in exercise 1 and include it properly.
+
+
+
+SOLUTION: a module is similar to a class in that it can store behaviours. However, modules cannot 
+instantiate objects-- they just hold things. 
+=end 
+
+module Pettable
+
+end
+
+def Animal 
+inlcude Pettable
+end
+
+my_cat = Cat.new 
