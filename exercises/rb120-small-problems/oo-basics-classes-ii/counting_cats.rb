870c74be2cac17b65372eda4f3e2b15fa50a97ba
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-21 19:14:00 -0700

    E- class methods and variables

---
+++ exercises/rb120-small-problems/oo-basics-classes-ii/counting_cats.rb
@@ -0,0 +1,26 @@
+=begin 
+Using the following code, create a class named Cat that 
+tracks the number of times a new Cat object is instantiated. 
+The total number of Cat instances should be printed when ::total is invoked.
+=end 
+
+class Cat 
+  attr_reader :total
+  @@number_of_cats = 0
+
+  def initialize
+    @@number_of_cats += 1
+  end
+
+  def self.total
+    @@number_of_cats
+  end
+end
+
+kitty1 = Cat.new
+kitty2 = Cat.new
+
+p Cat.total
+
+# expected 
+# 2
