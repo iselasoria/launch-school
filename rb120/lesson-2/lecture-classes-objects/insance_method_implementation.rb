ffa0593f312ddab24652738d1b98a46446caa3cd
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-21 17:31:44 -0700

    E- instance methods

---
+++ rb120/lesson-2/lecture-classes-objects/insance_method_implementation.rb
@@ -0,0 +1,28 @@
+=begin 
+Modify the class definition from above to facilitate the following methods. Note that there is no name= setter method now.
+
+bob = Person.new('Robert')
+bob.name                  # => 'Robert'
+bob.first_name            # => 'Robert'
+bob.last_name             # => ''
+bob.last_name = 'Smith'
+bob.name                  # => 'Robert Smith'
+
+
+
+Hint: let first_name and last_name be "states" and create an instance method called name that uses those states.
+=end 
+class Person
+  attr_accessor :first_name, :last_name
+
+  
+  def initialize(name)
+    parts = full_name.split
+    @first_name = parts.first 
+    @last_name = parts.size > 1 ? parts.last : ""
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+end
