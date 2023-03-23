67b1ffd7e37a8f67ec1e22ee02031b624cfe1115
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-23 15:06:45 -0700

    E- use self on setter methods to be explicit about what you're referencing

--- exercises/rb120-small-problems/easy-1/buggy_code_car_mileage.rb
+++ exercises/rb120-small-problems/easy-1/buggy_code_car_mileage.rb
@@ -10,7 +10,7 @@ class Car
   def increment_mileage(miles)
     total = mileage + miles
     self.mileage = total # we need to reference the setter method directly, this way we still make use of extended functionality in the setter
-  end # note that we could also do @mileage but when we bypass the setter method and go direc
+  end # note that we could also do @mileage but when we bypass the setter method and go directly to the instance var. Problematic if the setter has validation
 
   def print_mileage
     puts mileage
