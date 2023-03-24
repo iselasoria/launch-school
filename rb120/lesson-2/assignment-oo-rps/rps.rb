a037c21dcbc62c5f2053c484a1465029f929106b
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-23 23:32:55 -0700

    P- rps functionality

--- rb120/lesson-2/assignment-oo-rps/rps.rb
+++ rb120/lesson-2/assignment-oo-rps/rps.rb
@@ -47,11 +47,46 @@ class RPSGame
     puts "Thanks for playing, good bye!"
   end
 
+  def display_winner
+    puts "You chose #{human.move}."
+    puts "The computer chose #{computer.move}"
+    case human.move 
+    when 'rock'
+      puts "It's a tie!" if computer.move  == 'rock'
+      puts "You won!" if computer.move == 'scissors'
+      puts "Computer won!" if computer.move == 'paper'
+    when 'paper'
+      puts "It's a tie!" if computer.move  == 'paper'
+      puts "You won!" if computer.move == 'rock'
+      puts "Computer won!" if computer.move == 'scissors'
+    when 'scissors'
+      puts "It's a tie!" if computer.move  == 'scissors'
+      puts "You won!" if computer.move == 'paper'
+      puts "Computer won!" if computer.move == 'rock'
+    end
+  end
+
+  def play_again?
+    answer = nil 
+    
+    loop do 
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp 
+      break if ['y','n'].include?(answer.downcase)
+      puts "Sorry, you must choose y or n."
+    end
+    return true if answer == 'y'
+    return false 
+  end
+
   def play 
     display_welcome_message
+    loop do 
     human.choose 
     computer.choose 
     display_winner 
+    break unless play_again?
+    end
     display_goodbye_message
   end
 end
