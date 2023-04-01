b8add4fefe6719eb7dfd35dcdd3ddb0f63eb2c90
Rosa <rosaiselasoriamonzon@gmail.com>
2023-03-31 22:02:20 -0700

    displaying score as god intended

--- rb120/lesson-2/assignment-oo-rps/rps.rb
+++ rb120/lesson-2/assignment-oo-rps/rps.rb
@@ -35,10 +35,19 @@ class Move
 end
 
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :running_score
 
   def initialize
     set_name
+    @running_score = 0
+  end
+
+  def update_grand_score
+    @ultimate_score = Score.new
+  end
+
+  def display_running_score
+    puts "#{name}'s current score is: #{@running_score}"
   end
 end
 
@@ -77,25 +86,27 @@ class Computer < Player
 end
 
 class Score
-  attr_reader :score 
   # noun: score 
+  # no attr access because all the parts that touch this object are internal 
   # verbs: increments itself, reads from Move 
   # gives feedback to players
-  def score
-    # comes from move from player and computer
-    @score = # the move 
-    @wins = wins 
-  end
+  protected # we want a setter but we don't want to expose it 
 
-  def winning_score
+  def determine_winner
     # compares which is greater, borrows functionality from Move class 
     if human.move > computer.move
       puts "#{human.name} won!"
+      self.running_score = human.running_score += 1
     elsif human.move < computer.move
       puts "#{computer.name} won!"
+      self.running_score = computer.running_score += 1
     else
       puts "It's a tie!"
     end
+  end
+
+  def ultimate_winner
+    # is Score object the same as 10
   end
 
   def feedback
@@ -127,17 +138,19 @@ class RPSGame
   end
 
   def display_winner
-    # puts "#{human.name} chose #{human.move}."
-    # puts "#{computer.name} chose #{computer.move}"
+    puts "#{human.name} chose #{human.move}."
+    puts "#{computer.name} chose #{computer.move}"
 
-    ## todo this moved to the Score class
-    # if human.move > computer.move
-    #   puts "#{human.name} won!"
-    # elsif human.move < computer.move
-    #   puts "#{computer.name} won!"
-    # else
-    #   puts "It's a tie!"
-    # end
+    # todo this moved to the Score class
+    if human.move > computer.move
+      puts "#{human.name} won!"
+      human.running_score += 1
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+      computer.running_score += 1
+    else
+      puts "It's a tie!"
+    end
   end
 
   def play_again?
@@ -160,6 +173,8 @@ class RPSGame
       computer.choose
       display_move
       display_winner
+      human.display_running_score
+      computer.display_running_score
       break unless play_again?
     end
     display_goodbye_message
