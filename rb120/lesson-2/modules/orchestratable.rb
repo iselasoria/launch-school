871426594936622c9ff5b86017e4fbdfc79a361d
Rosa <rosaiselasoriamonzon@gmail.com>
2023-04-07 18:19:18 -0700

    pft- adding opponents

--- rb120/lesson-2/modules/orchestratable.rb
+++ rb120/lesson-2/modules/orchestratable.rb
@@ -12,6 +12,28 @@ module Orchestratable
     # puts "\n"
   end
 
+  def display_oponent_secret
+    case computer.name
+    when 'Terminator' then slow_display(MESSAGES["opponents"]["intro"]["Terminator"])
+    when 'Rusty' then slow_display(MESSAGES["opponents"]["intro"]["Rusty"])
+    when 'Chappie' then slow_display(MESSAGES["opponents"]["intro"]["Chappie"])
+    when 'Rosie Jetson' then slow_display(MESSAGES["opponents"]["intro"]["Rosie_Jetson"])
+    when 'Andy Roid' then slow_display(MESSAGES["opponents"]["intro"]["Andy_Roid"])
+    when 'Mr. Roboto' then slow_display(MESSAGES["opponents"]["intro"]["Mr.Roboto"])
+    end
+  end
+  def display_oponent_face
+    case computer.name
+    when 'Terminator' then puts MESSAGES["opponents"]["ascii"]["Terminator"]
+    when 'Rusty' then puts MESSAGES["opponents"]["ascii"]["Rusty"]
+    when 'Chappie' then puts MESSAGES["opponents"]["ascii"]["Chappie"]
+    when 'Rosie Jetson' then puts MESSAGES["opponents"]["ascii"]["Rosie_Jetson"]
+    when 'Andy Roid' then puts MESSAGES["opponents"]["ascii"]["Andy_Roid"]
+    when 'Mr. Roboto' then puts MESSAGES["opponents"]["ascii"]["Mr.Roboto"]
+    end
+  end
+
+
   def display_move
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
