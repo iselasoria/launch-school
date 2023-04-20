eddb3c1fe6b6e76cb6284d36f09ab4e8245ceb62
Rosa <rosaiselasoriamonzon@gmail.com>
2023-04-20 16:06:40 -0700

    E- debugging larger programs

--- exercises/rb120-small-problems/debugging/rock_or_not.rb
+++ exercises/rb120-small-problems/debugging/rock_or_not.rb
@@ -1,4 +1,4 @@
-
+# TODO Come back to review
 # We discovered Gary Bernhardt's repository for finding out whether something rocks or not, and decided to adapt it for a simple example.
 
 # In order to test the case when authentication fails, we can simply set API_KEY to any string other than the correct key.
