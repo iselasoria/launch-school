a8372620b390becc981d1bee2e11aa55b0ce73e6
Rosa <rosaiselasoriamonzon@gmail.com>
2023-04-20 15:05:14 -0700

    E- exercises on debugging, comparable module

---
+++ exercises/rb120-small-problems/debugging/sorting_distances.rb
@@ -0,0 +1,139 @@
+=begin
+When attempting to sort an array of various lengths, we are surprised to see
+that an ArgumentError is raised. Make the necessary changes to our code so
+that the various lengths can be properly sorted and line 62 produces the expected output.
+
+=end
+
+class Length
+  attr_reader :value, :unit
+
+  def initialize(value, unit)
+    @value = value
+    @unit = unit
+  end
+
+  def as_kilometers
+    convert_to(:km, { km: 1, mi: 0.6213711, nmi: 0.539957 })
+  end
+
+  def as_miles
+    convert_to(:mi, { km: 1.609344, mi: 1, nmi: 0.8689762419 })
+  end
+
+  def as_nautical_miles
+    convert_to(:nmi, { km: 1.8519993, mi: 1.15078, nmi: 1 })
+  end
+
+  def <=>(other) # this method got added
+    case unit
+    when :km  then value <=> other.as_kilometers.value
+    when :mi  then value <=> other.as_miles.value
+    when :nmi then value <=> other.as_nautical_miles.value
+    end
+  end
+
+  def ==(other)
+    case unit
+    when :km  then value == other.as_kilometers.value
+    when :mi  then value == other.as_miles.value
+    when :nmi then value == other.as_nautical_miles.value
+    end
+  end
+
+  def <(other)
+    case unit
+    when :km  then value < other.as_kilometers.value
+    when :mi  then value < other.as_miles.value
+    when :nmi then value < other.as_nautical_miles.value
+    end
+  end
+
+  def <=(other)
+    self < other || self == other
+  end
+
+  def >(other)
+    !(self <= other)
+  end
+
+  def >=(other)
+    self > other || self == other
+  end
+
+  def to_s
+    "#{value} #{unit}"
+  end
+
+  private
+
+  def convert_to(target_unit, conversion_factors)
+    Length.new((value / conversion_factors[unit]).round(4), target_unit)
+  end
+end
+
+# Example
+
+puts [Length.new(1, :mi), Length.new(1, :nmi), Length.new(1, :km)].sort
+# => comparison of Length with Length failed (ArgumentError)
+# expected output:
+# 1 km
+# 1 mi
+# 1 nmi
+
+# S:
+# Since we are comparing objects of a custom class, Length, we need Length objects to have
+# access to the <=> method to compare. Since it does not, that is why we run into the error.
+# Now that we have the spaceship operator, we can even clean the code further like this:
+
+class Length
+  include Comparable
+  attr_reader :value, :unit
+
+  def initialize(value, unit)
+    @value = value
+    @unit = unit
+  end
+
+  def as_kilometers
+    convert_to(:km, { km: 1, mi: 0.6213711, nmi: 0.539957 })
+  end
+
+  def as_miles
+    convert_to(:mi, { km: 1.609344, mi: 1, nmi: 0.8689762419 })
+  end
+
+  def as_nautical_miles
+    convert_to(:nmi, { km: 1.8519993, mi: 1.15078, nmi: 1 })
+  end
+
+  def <=>(other)
+    case unit
+    when :km  then value <=> other.as_kilometers.value
+    when :mi  then value <=> other.as_miles.value
+    when :nmi then value <=> other.as_nautical_miles.value
+    end
+  end
+
+  def to_s
+    "#{value} #{unit}"
+  end
+
+  private
+
+  def convert_to(target_unit, conversion_factors)
+    Length.new((value / conversion_factors[unit]).round(4), target_unit)
+  end
+end
+
+
+puts [Length.new(1, :mi), Length.new(1, :nmi), Length.new(1, :km)].sort
+
+# Further exploration:
+# floating point numbers introduce imprecision:
+Length.new(10, :km).as_nautical_miles.as_kilometers == Length.new(10, :km) # => false
+
+# for this reason, they typically don't check for equality but rather whether they are
+# within an acceptable range of tolerance. The code below assumes we've implemented the
+# Length#- metjos as well:
+(Length.new(10, :km).as_nautical_miles.as_kilometers - Length.new(10, :km)).abs < 0.001 # => true
