bf1c5045bcaaf4cb037ab3f590a3e09068c655e5
Rosa <rosaiselasoriamonzon@gmail.com>
2023-08-21 20:46:49 -0700

    e-exercises restructured

---
+++ rb120/second_pass/exercises/oo_basics_ii_classes_objects_2/generic_greeting_i.rb
@@ -0,0 +1,10 @@
+# Modify the following code so that Hello! I'm a cat! is printed when Cat.generic_greeting is invoked.
+
+class Cat
+end
+
+Cat.generic_greeting
+
+# expected output:
+
+# Hello! I'm a cat!
